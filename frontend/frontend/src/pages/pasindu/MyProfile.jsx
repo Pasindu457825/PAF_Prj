f58ffd7f17c53b51b98c3da9596bee9110a31d97
LakshaniKarunathilaka <153898635+LakshaniKarunathilaka@users.noreply.github.com>
2025-04-25 23:51:04 +0530

    Update MyProfile.jsx

--- frontend/frontend/src/pages/pasindu/MyProfile.jsx
+++ frontend/frontend/src/pages/pasindu/MyProfile.jsx
@@ -68,11 +68,11 @@ const MyProfile = () => {
               }}
               className="py-2 px-4 bg-red-500 hover:bg-red-600 text-white rounded-lg shadow transition"
             >
-              My Notificatin
+              My Notification
         </button>
         <button
           className="mt-4 w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-md transition"
-          onClick={() => navigate("/followdUsers")}
+          onClick={() => navigate("/followUsers")}
         >
           My Friend
         </button>
